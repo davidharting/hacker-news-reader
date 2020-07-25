1c9897d591990288520e355b6cff643c2e600975
David Harting <davideharting@gmail.com>
2020-07-25 12:22:54 -0400

    cache data in local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import storiesReducer from "features/stories-list/storiesSlice";
 
+const LOCAL_STORAGE_STATE_CACHE_KEY = "hacker-news-reader-redux-state";
+
 export const store = configureStore({
+  preloadedState: loadState(),
   reducer: {
     stories: storiesReducer,
   },
@@ -14,3 +17,27 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+// Approach for persisting state came from Dan Abramov himself 🙏
+// https://egghead.io/lessons/javascript-redux-persisting-the-state-to-the-local-storage
+function saveState(state: { stories: Object }) {
+  try {
+    const serializedState = JSON.stringify({ stories: state.stories });
+    localStorage.setItem(LOCAL_STORAGE_STATE_CACHE_KEY, serializedState);
+  } catch (err) {
+    console.error("Unable to persist state to local storage.", err);
+  }
+}
+
+function loadState() {
+  try {
+    const serializedState = localStorage.getItem(LOCAL_STORAGE_STATE_CACHE_KEY);
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch (err) {
+    return undefined;
+  }
+}
