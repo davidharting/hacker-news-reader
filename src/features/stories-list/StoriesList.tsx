c8f2dcfabf58f090ba32f77165e2c9257dfadf6a
David Harting <david.e.harting@gmail.com>
2020-07-26 16:16:07 -0400

    Refactors and cleanup (#4)
    * Improve mobile experience
    * Simplify hooks for fetching and paging stories

--- src/features/stories-list/StoriesList.tsx
+++ src/features/stories-list/StoriesList.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNetworkStatus } from "browser/network";
 import { useScrolledToBottom } from "browser/window";
-import Spinner from "elements/spinner";
+import Spinner from "elements/spinner/index";
 import {
   fetchMaxItem,
   fetchNextStory,
   selectCanFetch,
   selectOldestStoryId,
-  selectPageNumber,
   selectDescendingStories,
   pageForward,
   selectCurrentPageStatus,
@@ -19,10 +18,7 @@ import Refresh from "./components/Refresh";
 import styles from "./stories-list.module.css";
 
 function StoriesList({ pageSize }: StoriesListProps) {
-  const stories = useSelector(selectDescendingStories);
-  // TODO: This hook should simply return the stories
-  useStoryPage(pageSize);
-
+  const stories = useInfiniteScrollStories(pageSize);
   const pageStatus = useSelector(selectCurrentPageStatus(pageSize));
 
   return (
@@ -50,44 +46,57 @@ interface StoriesListProps {
   pageSize: number;
 }
 
-function useStoryPage(pageSize: number) {
+/**
+ * This hook is responsible for fetching an infinitely scrolling feed of hacker news stories.
+ * It achieves this by first finding the current maximum HN item ID, and then walking backwards through items to fill up a "page."
+ * When the user scrolls to the bottom of the feed, the next page will be fetched if the current page is complete.
+ * @param pageSize
+ */
+function useInfiniteScrollStories(pageSize: number) {
+  useMaxItemId();
   const dispatch = useDispatch();
+
   const stories = useSelector(selectDescendingStories);
   const canFetch = useSelector(selectCanFetch);
+  const pageStatus = useSelector(selectCurrentPageStatus(pageSize));
   const oldestStoryId = useSelector(selectOldestStoryId);
-  const maxItemId = useSelector(selectMaxItemId);
-  const pageNumber = useSelector(selectPageNumber);
 
   const networkStatus = useNetworkStatus();
   const scrolledToBottom = useScrolledToBottom();
 
   React.useEffect(() => {
-    if (!maxItemId) {
-      dispatch(fetchMaxItem());
-    }
-  }, [dispatch, maxItemId]);
-
-  React.useEffect(() => {
-    if (
-      networkStatus === "online" &&
-      canFetch &&
-      stories.length < pageSize * (pageNumber + 1)
-    ) {
+    if (networkStatus === "online" && canFetch && pageStatus === "INCOMPLETE") {
       dispatch(fetchNextStory());
     }
   }, [
     dispatch,
     canFetch,
-    oldestStoryId,
+    oldestStoryId, // We know we are ready to fetch a new story when the oldest known story ID changes
     networkStatus,
-    pageSize,
-    stories.length,
-    pageNumber,
+    pageStatus,
   ]);
 
   React.useEffect(() => {
     if (scrolledToBottom) {
       dispatch(pageForward(pageSize));
     }
-  }, [dispatch, pageSize, scrolledToBottom, stories.length]);
+  }, [
+    dispatch,
+    pageSize,
+    scrolledToBottom,
+    stories.length, // If the user is already at the bottom of the page when the last story of the current page comes in, we want to start fetching the next page
+  ]);
+
+  return stories;
+}
+
+function useMaxItemId() {
+  const dispatch = useDispatch();
+  const maxItemId = useSelector(selectMaxItemId);
+
+  React.useEffect(() => {
+    if (!maxItemId) {
+      dispatch(fetchMaxItem());
+    }
+  }, [dispatch, maxItemId]);
 }
